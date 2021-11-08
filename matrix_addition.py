20d47cfc522e919e15b2dd2237766af7766c687a
RunDECT <93350655+RunDECT@users.noreply.github.com>
2021-11-09 01:55:30 +0300

    Create matrix_addition.py

---
+++ matrix_addition.py
@@ -0,0 +1,14 @@
+n, m = [int(i) for i in input().split()]
+matrix_a = [[int(i) for i in input().split()] for _ in range(n)]
+input()
+matrix_b = [[int(i) for i in input().split()] for _ in range(n)]
+matrix_c = [[0] * m for _ in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        matrix_c[i][j] = matrix_a[i][j] + matrix_b[i][j]
+
+for i in range(n):
+    for j in range(m):
+        print(str(matrix_c[i][j]).ljust(1), end=' ')
+    print()
