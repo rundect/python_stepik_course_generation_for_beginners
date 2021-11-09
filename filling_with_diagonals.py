1fa11d2da514ff6deb2b3bbfa2d0e44a72a85e7a
RunDECT <93350655+RunDECT@users.noreply.github.com>
2021-11-10 01:18:57 +0300

    Update filling_with_diagonals.py

--- filling_with_diagonals.py
+++ filling_with_diagonals.py
@@ -2,19 +2,21 @@
 n, m = [int(i) for i in input().split()]
 matrix = [[0] * m for _ in range(n)]
 
+counter = 1
 row = 1
-minus = 1
+term = 0
 for i in range(n):
-    counter = row
     for j in range(m):
         if j <= n - i - 1:
-            counter += (j + i)
             matrix[i][j] = counter
+            term += 1
         else:
-            counter += (j - )
-            matrix[i][j] = counter
-    row += (i + 1)
-
+            counter += j
+            matrix[i][j] = 0
+        counter += term
+    row += (i + 2)
+    term -= 3
+    counter = row
 
 for i in range(n):
     for j in range(m):
